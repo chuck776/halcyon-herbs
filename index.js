e9d269d21500bae03b724e8d4f617240bfc42db1
chuck776 <u22567659@tuks.gmail.com>
2024-12-11 16:28:32 +0200

    refractored for the clothing page

--- index.js
+++ index.js
@@ -30,3 +30,14 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+function clothingbar(){
+    let btn = document.querySelector("#btn");
+    let sidebar = document.querySelector(".sidebar");
+
+    btn.onclick = function(){
+        sidebar.classList.toggle("active");
+
+    }
+}
+ clothingbar();
+
